0f7e409ac678a0990fa138f41a7b7fda5458b77c
Joao Vieira <joao.pedro100@hotmail.com.br>
2022-05-17 15:08:22 -0300

    Route error small fix

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,9 +3,7 @@ import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-ada
 import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository';
 import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 
-export const routes = express.Router()
-
-
+export const routes = express.Router();
 
 routes.post('/feedbacks', async (req, res) => {
     const { type, comment, screenshot } = req.body;
